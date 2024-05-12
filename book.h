e4f3c532705d1d106b945307a365fb98db955370
raunakkk21 <143111163+raunakkk21@users.noreply.github.com>
2024-05-12 15:18:31 +0530

    Create book.h

---
+++ book.h
@@ -0,0 +1,17 @@
+#ifndef BOOK_H
+#define BOOK_H
+
+#include <string>
+
+class Book {
+private:
+    std::string title;
+    std::string author;
+    bool available;
+
+public:
+    // Constructor, getters, setters
+};
+
+#endif
+
