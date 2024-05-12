a244c784e5a67552d98f801e21767a36c0be5503
raunakkk21 <143111163+raunakkk21@users.noreply.github.com>
2024-05-12 15:18:00 +0530

    Create library.cpp

---
+++ library.cpp
@@ -0,0 +1,28 @@
+#include "Library.h"
+#include <iostream>
+
+using namespace std;
+
+void Library::addBook() {
+    // Implementation to add a book
+}
+
+void Library::removeBook() {
+    // Implementation to remove a book
+}
+
+void Library::displayBooks() {
+    // Implementation to display all books
+}
+
+void Library::issueBook() {
+    // Implementation to issue a book to a student
+}
+
+void Library::returnBook() {
+    // Implementation to return a book
+}
+
+void Library::displayIssuedBooks() {
+    // Implementation to display issued books
+}
