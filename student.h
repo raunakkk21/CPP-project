c70efa4ae28fbea18399985b1e5ce36886769b5a
raunakkk21 <143111163+raunakkk21@users.noreply.github.com>
2024-05-12 15:20:10 +0530

    Create student.h

---
+++ student.h
@@ -0,0 +1,16 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <string>
+
+class Student {
+private:
+    std::string name;
+    int id;
+    // Additional student information
+
+public:
+    // Constructor, getters, setters
+};
+
+#endif
